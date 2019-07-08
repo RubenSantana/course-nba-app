44a480617c73695f848178310dec68018c72b0c0
RubenSantana <ruben_santana1990@hotmail.com>
2019-07-08 18:36:43 +0100

    Added the sign-out option to the sidenav

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import style from "./header.css";
 import FontAwesome from "react-fontawesome";
+
+import style from "./header.css";
 import SideNav from "./SideNav/sideNav";
 
 const Header = props => {
--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 
 // COMPONENTS
@@ -10,22 +10,19 @@ import NewsMain from "./components/Articles/News/Main/index";
 import VideosMain from "./components/Articles/Videos/Main/index";
 import SignIn from "./components/SignIn/signin";
 
-class Routes extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <Layout>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/news" exact component={NewsMain} />
-          <Route path="/articles/:id" exact component={NewsArticle} />
-          <Route path="/videos/:id" exact component={VideoArticle} />
-          <Route path="/videos" exact component={VideosMain} />
-          <Route path="/sign-in" exact component={SignIn} />
-        </Switch>
-      </Layout>
-    );
-  }
-}
+const Routes = props => {
+  return (
+    <Layout user={props.user}>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/news" exact component={NewsMain} />
+        <Route path="/articles/:id" exact component={NewsArticle} />
+        <Route path="/videos/:id" exact component={VideoArticle} />
+        <Route path="/videos" exact component={VideosMain} />
+        <Route path="/sign-in" exact component={SignIn} />
+      </Switch>
+    </Layout>
+  );
+};
 
 export default Routes;
