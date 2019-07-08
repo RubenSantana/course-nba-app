5873fd02009e9b8b2b45ae6b2844f0b427f1194c
RubenSantana <ruben_santana1990@hotmail.com>
2019-07-08 13:49:40 +0100

    Changed the backend database access from axios+db.json to Google Firebase in in the videos and post views components

--- src/components/Articles/Elements/postData.js
+++ src/components/Articles/Elements/postData.js
@@ -1,11 +1,17 @@
 import React from "react";
+import moment from "moment";
+
 import styles from "../articles.css";
+
+const formatDate = date => {
+  return moment(date).format(" MM-DD-YYYY");
+};
 
 const PostData = props => (
   <div className={styles.articlePostData}>
     <div>
       Date:
-      <span>{props.data.date}</span>
+      <span>{formatDate(props.data.date)}</span>
     </div>
     <div>
       Author:
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -12,9 +12,7 @@ const Home = () => (
       settings={{ dots: false }}
     />
     <NewsList type="card" loadMore={true} start={3} amount={3} />
-    {/*
     <VideosList type="card" title={true} loadmore={true} start={0} amount={3} />
-    */}
   </div>
 );
 
