3c426808558b2fec19589070fb98dc588dd1d210
RubenSantana <ruben_santana1990@hotmail.com>
2019-07-09 01:08:45 +0100

    Fixed the slider in order to show the images

--- src/components/widgets/NewsSlider/slider.js
+++ src/components/widgets/NewsSlider/slider.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import SliderTemplates from "./slider_templates";
-import { firebaseArticles, firebaseLooper } from "../../../firebase";
+import { firebase, firebaseArticles, firebaseLooper } from "../../../firebase";
 
 class NewsSlider extends Component {
   state = {
@@ -14,25 +14,41 @@ class NewsSlider extends Component {
       .once("value")
       .then(snapshot => {
         const news = firebaseLooper(snapshot);
-        this.setState({
-          news
-        });
-      });
 
-    {
-      /*
-    axios
-      .get(
-        `${URL}/articles?_start=${this.props.start}&_end=${this.props.start +
-          this.props.amount}`
-      )
-      .then(response => {
-        this.setState({
-          news: response.data
+        // news.forEach((item, i) => {
+        //   firebase
+        //     .storage()
+        //     .ref("images")
+        //     .child(item.image)
+        //     .getDownloadURL()
+        //     .then(url => {
+        //       news[i].image = url;
+        //       this.setState({ news });
+        //     });
+        // });
+
+        const asyncFunction = (item, i, callback) => {
+          firebase
+            .storage()
+            .ref("images")
+            .child(item.image)
+            .getDownloadURL()
+            .then(url => {
+              news[i].image = url;
+              callback();
+            });
+        };
+
+        let requests = news.map((item, i) => {
+          return new Promise(resolve => {
+            asyncFunction(item, i, resolve);
+          });
+        });
+
+        Promise.all(requests).then(() => {
+          this.setState({ news });
         });
       });
-      */
-    }
   }
 
   render() {
